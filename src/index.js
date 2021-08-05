78bb29e10b9b53b8bf8c4c085bbb52db878b7b00
Tyler Long <tyler4long@gmail.com>
2021-08-05 15:42:09 -0700

    First working version

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const rcsdk = new SDK({
   server: process.env.RINGCENTRAL_SERVER_URL,
   clientId: process.env.RINGCENTRAL_CLIENT_ID,
   clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
-  redirectUri
 });
 
 const platform = rcsdk.platform();
@@ -20,6 +19,17 @@ if(code === null) {
     redirectUri
   });
   console.log(loginUrl);
+  const link = document.createElement('a');
+  link.href = loginUrl;
+  link.innerText = 'Login';
+  document.body.appendChild(link);
 } else {
   // exchange code for token
+  (async () => {
+    const resp = await platform.login({code, redirect_uri: redirectUri});
+    const token = await resp.json();
+    console.log(token);
+    const r = await platform.get('/restapi/v1.0/account/~/extension/~/call-log');
+    console.log(await r.json());
+  })();
 }
