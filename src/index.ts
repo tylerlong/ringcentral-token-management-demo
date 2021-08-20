bd19aab337fa3f705f3272d8a9c291cd68551dcc
Tyler Long <tyler4long@gmail.com>
2021-08-19 18:22:01 -0700

    First fully working version

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import SDK from '@ringcentral/sdk';
+import SDK, {AuthData} from '@ringcentral/sdk';
+import {events} from '@ringcentral/sdk/lib/platform/Platform';
 import localforage from 'localforage';
 
-const authCodeKey = 'ringcentral-token-management-demo-auth-code'
-const tokenKey = 'ringcentral-token-management-demo-token'
-
+// global constants
+const authCodeKey = 'ringcentral-token-management-demo-auth-code';
+const tokenKey = 'ringcentral-token-management-demo-token';
 const redirectUri = window.location.origin + window.location.pathname;
 
+// initiate RingCentral SDK
 const rcsdk = new SDK({
   server: process.env.RINGCENTRAL_SERVER_URL,
   clientId: process.env.RINGCENTRAL_CLIENT_ID,
@@ -13,22 +15,28 @@ const rcsdk = new SDK({
 });
 const platform = rcsdk.platform();
 
+// save token whenever login success or refresh success
+const saveToken = async (res: Response) => {
+  const token = await res.clone().json();
+  await localforage.setItem(tokenKey, token);
+};
+platform.on(events.refreshSuccess, async res => {
+  saveToken(res);
+});
+platform.on(events.loginSuccess, async res => {
+  saveToken(res);
+});
 
+// fetch call logs
+const fetchCallLogs = async () => {
+  const r = await platform.get('/restapi/v1.0/account/~/extension/~/call-log');
+  console.log(await r.json());
+};
 
-(async () => {
-
-  const urlSearchParams = new URLSearchParams(new URL(window.location.href).search);
-const code = urlSearchParams.get('code');
-if(code !== null) {
-  await localforage.setItem(authCodeKey, code);
-}
-
-})();
-
-if(code === null) {
-  // login
+// add login link to page to let user click to login
+const addLoginLink = () => {
   const loginUrl = platform.loginUrl({
-    redirectUri
+    redirectUri,
   });
   console.log(loginUrl);
   const link = document.createElement('a');
@@ -36,18 +44,47 @@ if(code === null) {
   link.innerText = 'Login';
   document.body.appendChild(link);
   link.onclick = () => {
-    const popupWindow = window.open(loginUrl, 'windowName', "width=300,height=400");
-    setInterval(() => {
-      console.log(popupWindow.location.href);
-    }, 1000)
+    const popupWindow = window.open(
+      loginUrl,
+      'windowName',
+      'width=300,height=400'
+    );
+    const handle = setInterval(async () => {
+      const code = (await localforage.getItem(authCodeKey)) as string;
+      if (code !== null) {
+        popupWindow?.close();
+        link.remove();
+        clearInterval(handle);
+        await platform.login({code, redirect_uri: redirectUri});
+        await fetchCallLogs();
+      }
+    }, 1000);
   };
-} else {
-  // exchange code for token
-  (async () => {
-    const resp = await platform.login({code, redirect_uri: redirectUri});
-    const token = await resp.json();
-    console.log(token);
-    const r = await platform.get('/restapi/v1.0/account/~/extension/~/call-log');
-    console.log(await r.json());
-  })();
-}
+};
+
+(async () => {
+  // fetch code from query parameters and save it
+  const urlSearchParams = new URLSearchParams(
+    new URL(window.location.href).search
+  );
+  const code = urlSearchParams.get('code');
+  if (code !== null) {
+    await localforage.setItem(authCodeKey, code);
+    return; // because this is the popup window.
+  }
+
+  // load saved token and fetch call logs.
+  const token = (await localforage.getItem(tokenKey)) as AuthData;
+  if (token === null) {
+    addLoginLink();
+  } else {
+    platform.auth().setData(token);
+    try {
+      await platform.refresh();
+      await fetchCallLogs();
+    } catch (e) {
+      console.log(e); // most likely refresh token expired
+      addLoginLink();
+    }
+  }
+})();
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,16 +1,30 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const dotenv = require('dotenv-override-true');
-const {DefinePlugin} = require('webpack');
+/* eslint-disable node/no-unpublished-require */
+/* eslint-disable node/no-unpublished-import */
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import dotenv from 'dotenv-override-true';
+import {DefinePlugin, Configuration} from 'webpack';
 
-const config = {
-  mode: "development",
+const config: Configuration = {
+  mode: 'development',
   devtool: 'source-map',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+    ],
+  },
   resolve: {
     fallback: {
-       "crypto": require.resolve("crypto-browserify"),
-       "buffer": require.resolve("buffer/"),
-       "stream": require.resolve("stream-browserify"),
-      }
+      crypto: require.resolve('crypto-browserify'),
+      buffer: require.resolve('buffer/'),
+      stream: require.resolve('stream-browserify'),
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -20,6 +34,6 @@ const config = {
       'process.env': JSON.stringify(dotenv.config().parsed),
     }),
   ],
-}
+};
 
-module.exports = config;
+export default [config];
