8f3ccfb132cff019eac7e88724de07d43ccfbbd4
Tyler Long <tyler4long@gmail.com>
2021-08-19 17:09:39 -0700

    Try TS

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import SDK from '@ringcentral/sdk';
+import localforage from 'localforage';
+
+const authCodeKey = 'ringcentral-token-management-demo-auth-code'
+const tokenKey = 'ringcentral-token-management-demo-token'
 
 const redirectUri = window.location.origin + window.location.pathname;
 
@@ -7,11 +11,19 @@ const rcsdk = new SDK({
   clientId: process.env.RINGCENTRAL_CLIENT_ID,
   clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
 });
-
 const platform = rcsdk.platform();
 
-const urlSearchParams = new URLSearchParams(new URL(window.location.href).search);
+
+
+(async () => {
+
+  const urlSearchParams = new URLSearchParams(new URL(window.location.href).search);
 const code = urlSearchParams.get('code');
+if(code !== null) {
+  await localforage.setItem(authCodeKey, code);
+}
+
+})();
 
 if(code === null) {
   // login
@@ -20,9 +32,15 @@ if(code === null) {
   });
   console.log(loginUrl);
   const link = document.createElement('a');
-  link.href = loginUrl;
+  link.href = '#';
   link.innerText = 'Login';
   document.body.appendChild(link);
+  link.onclick = () => {
+    const popupWindow = window.open(loginUrl, 'windowName', "width=300,height=400");
+    setInterval(() => {
+      console.log(popupWindow.location.href);
+    }, 1000)
+  };
 } else {
   // exchange code for token
   (async () => {
